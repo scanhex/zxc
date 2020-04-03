677841840f2f0491277eaf31a6a9916e8fdb1075
Maxim <maksukh@inbox.ru>
2020-04-03 21:35:17 +0300

    Update GameState + add microtest

--- src/Game/GameState.cpp
+++ src/Game/GameState.cpp
@@ -1,4 +1,5 @@
 #include "GameState.h"
+#include "GameStateServer.h"
 
 GameState::GameState() {
     StatsBuilder heroStatsBuilder = StatsBuilder().
@@ -16,6 +17,11 @@ GameState::GameState() {
     secondHero = new Hero(heroStatsBuilder.create(), Point(100, 100));
 }
 
+GameState::~GameState() {
+    delete firstHero;
+    delete secondHero;
+}
+
 double GameState::getHealthPoints(Player player) {
     switch (player) {
         case Player::First:
@@ -31,8 +37,10 @@ void GameState::setHealthPoints(double amount, Player player) {
     switch (player) {
         case Player::First:
             firstHero->setHealthPoints(amount);
+            break;
         case Player::Second:
             secondHero->setHealthPoints(amount);
+            break;
         default:
             assert(false);
     }
@@ -46,22 +54,100 @@ bool GameState::gameIsFinished() {
 GameStateServer::GameStateServer(double gameTick) : GameState(), gameTick_{gameTick} {}
 
 void GameStateServer::update() {
-    // only hp regen for now
+    // only regen for now
     double healPerTick = firstHero->getHpRegen() * gameTick_;
-    firstHero->heal(healPerTick);
+    double manaPerTick = firstHero->getMpRegen() * gameTick_;
+    firstHero->applyHeal(healPerTick);
+    firstHero->regenMana(manaPerTick);
 
     healPerTick = secondHero->getHpRegen() * gameTick_;
-    secondHero->heal(healPerTick);
+    manaPerTick = secondHero->getMpRegen() * gameTick_;
+    secondHero->applyHeal(healPerTick);
+    secondHero->regenMana(manaPerTick);
 }
 
-void GameStateServer::damage(double amount, Player player) {
+void GameStateServer::applyDamage(double amount, Player player) {
     switch (player) {
         case Player::First:
-            firstHero->damage(amount);
+            firstHero->applyDamage(amount);
             break;
         case Player::Second:
-            secondHero->damage(amount);
+            secondHero->applyDamage(amount);
             break;
+        default:
+            assert(false);
+    }
+}
+
+void GameStateServer::applyDamagePhys(double amount, Player player) {
+    switch (player) {
+        case Player::First:
+            firstHero->applyDamagePhys(amount);
+            break;
+        case Player::Second:
+            secondHero->applyDamagePhys(amount);
+            break;
+        default:
+            assert(false);
+    }
+}
+
+void GameStateServer::applyDamageMagic(double amount, Player player) {
+    switch (player) {
+        case Player::First:
+            firstHero->applyDamageMagic(amount);
+            break;
+        case Player::Second:
+            secondHero->applyDamageMagic(amount);
+            break;
+        default:
+            assert(false);
+    }
+}
+
+void GameStateServer::regenMana(double amount, Player player) {
+    switch (player) {
+        case Player::First:
+            firstHero->regenMana(amount);
+            break;
+        case Player::Second:
+            secondHero->regenMana(amount);
+            break;
+        default:
+            assert(false);
+    }
+}
+
+void GameStateServer::spendMana(double amount, Player player) {
+    switch (player) {
+        case Player::First:
+            firstHero->spendMana(amount);
+            break;
+        case Player::Second:
+            secondHero->spendMana(amount);
+            break;
+        default:
+            assert(false);
+    }
+}
+
+bool GameStateServer::canSpendMana(double amount, Player player) const {
+    switch (player) {
+        case Player::First:
+            return firstHero->canSpendMana(amount);
+        case Player::Second:
+            return secondHero->canSpendMana(amount);
+        default:
+            assert(false);
+    }
+}
+
+bool GameStateServer::isDead(Player player) const {
+    switch (player) {
+        case Player::First:
+            return firstHero->isDead();
+        case Player::Second:
+            return secondHero->isDead();
         default:
             assert(false);
     }
