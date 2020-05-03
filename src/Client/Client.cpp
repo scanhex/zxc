7e321f73cbaddabd827764bb72bb2d92b89fbf7c
Alexander Morozov <scanhex@gmail.com>
2020-05-03 20:38:23 +0300

    Fix unused parameters

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -1,4 +1,5 @@
 #include "Client.h"
+#include "Utils/Utils.h"
 
 extern bool exit_flag;
 
@@ -55,7 +56,8 @@ void Client::ConnectionToServer::waitForGameStart() {
                BIND_FN2(handleWaitRead, std::placeholders::_1, std::placeholders::_2));
 }
 
-void Client::ConnectionToServer::handleWriteToSocket(const boost::system::error_code &err, __attribute__ ((unused)) size_t bytes) {
+void Client::ConnectionToServer::handleWriteToSocket(const boost::system::error_code &err, size_t bytes) {
+    ignore(bytes);
     if (err || exit_flag) {
         stopConnection();
         return;
@@ -75,7 +77,8 @@ void Client::ConnectionToServer::writeToSocket() {
                            BIND_FN2(handleWriteToSocket, std::placeholders::_1, std::placeholders::_2));
 }
 
-size_t Client::ConnectionToServer::checkWaitReadComplete(const boost::system::error_code &err, __attribute__ ((unused)) size_t bytes) {
+size_t Client::ConnectionToServer::checkWaitReadComplete(const boost::system::error_code &err, size_t bytes) {
+    ignore(bytes);
     if (err || exit_flag) {
         stopConnection();
         return 0;
@@ -84,7 +87,8 @@ size_t Client::ConnectionToServer::checkWaitReadComplete(const boost::system::er
     return done ? 0 : 1;
 }
 
-void Client::ConnectionToServer::handleWaitRead(const boost::system::error_code &err, __attribute__ ((unused)) size_t bytes) {
+void Client::ConnectionToServer::handleWaitRead(const boost::system::error_code &err,  size_t bytes) {
+    ignore(bytes);
     if (err || exit_flag) {
         stopConnection();
         return;
@@ -97,7 +101,8 @@ void Client::ConnectionToServer::handleWaitRead(const boost::system::error_code 
     }
 }
 
-void Client::ConnectionToServer::handleReadFromSocket(const boost::system::error_code &err, __attribute__ ((unused)) size_t bytes) {
+void Client::ConnectionToServer::handleReadFromSocket(const boost::system::error_code &err, size_t bytes) {
+    ignore(bytes);
     if (err || exit_flag) {
         stopConnection();
         return;
