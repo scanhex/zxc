e5ac5ef3ea5bab663a6096c33ec32031f99662ad
Maxim <maksukh@inbox.ru>
2020-05-20 21:35:47 +0300

    Fix balance

--- src/Game/Creep.cpp
+++ src/Game/Creep.cpp
@@ -25,7 +25,7 @@ Position Creep::creepSpawns_[] = {Position(Point(-3, -6), 0),  // Radiant
 Creep::Creep(Team team) : Unit(defaultCreepStatsBuilder_.create(), creepSpawns_[static_cast<uint8_t>(team)]) {
     team_ = team;
     goldKillReward_ = 100;
-    expKillReward_ = 500;
+    expKillReward_ = 1000;
     giveId();
 }
 
